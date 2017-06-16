f48afa6c79d82a0b7c58960c409c5ab9c8714a3a
Vladimir Petrigo <taenaru@gmail.com>
2017-06-16 09:18:27 +0300

    Final fix

--- task04/coding_style.c
+++ task04/coding_style.c
@@ -8,7 +8,7 @@ int do_work(int *my_int, int retval)
 	int x;
 	int y = *my_int;
 	int z;
-	
+
 	for (x = 0; x < *my_int; ++x)
 		udelay(10);
 
@@ -19,7 +19,6 @@ int do_work(int *my_int, int retval)
 		pr_debug("We slept a long time!");
 
 	z = x * y;
-
 	return z;
 }
 
@@ -28,7 +27,6 @@ int my_init(void)
 	int x = 10;
 
 	x = do_work(&x, x);
-
 	return x;
 }
 
