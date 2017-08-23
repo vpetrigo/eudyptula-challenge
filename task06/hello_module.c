82c341d87aaf81116b07ca48494ff66300e9ee7f
Vladimir Petrigo <taenaru@gmail.com>
2017-08-23 20:06:51 +0300

    Fix miscdevice module

--- task06/hello_module.c
+++ task06/hello_module.c
@@ -15,13 +15,11 @@ MODULE_LICENSE("Dual BSD/GPL");
 #define DEV_NAME "eudyptula"
 #define BUF_SIZE 32
 
-static int hello_open(struct inode *, struct file *);
-static ssize_t hello_read(struct file *, char __user *, size_t, loff_t *);
-static ssize_t hello_write(struct file *, const char __user *, size_t, loff_t *);
+static ssize_t hello_write(struct file *filp, const char __user *from, size_t length, loff_t *pos);
+static ssize_t hello_read(struct file *filp, char __user *to, size_t length, loff_t *pos);
 
 static const struct file_operations hello_misc_ops = {
 	.owner = THIS_MODULE,
-	.open = hello_open,
 	.read = hello_read,
 	.write = hello_write
 };
@@ -32,27 +30,21 @@ static struct miscdevice hello_mdev = {
 	.fops = &hello_misc_ops
 };
 
-static int hello_open(struct inode *inode, struct file *filp)
-{
-	pr_debug("hello_module: hello open\n");
-	filp->private_data = &inode->i_rdev;
-	pr_debug("hello_module: open miscdevice with minor ID %d\n", MINOR(inode->i_rdev));
-
-	return 0;
-}
-
 static ssize_t hello_read(struct file *filp, char __user *to, size_t length, loff_t *pos)
 {
 	char buf[BUF_SIZE] = {'\0'};
-	const dev_t *devp = filp->private_data;
 	ssize_t result = 0;
 	
-	result = snprintf(buf, sizeof(buf), "%d\n", MINOR(*devp));
+	result = snprintf(buf, sizeof(buf), "%d\n", hello_mdev.minor);
 
-	if (result < 0)
-		goto fail;	
+	if (result < 0 || result > length)
+    {
+        result = -EFAULT;
+		goto fail;
+    }
 
-	if (copy_to_user(to, buf, result)) {
+	if (copy_to_user(to, buf, result))
+    {
 		result = -EFAULT;
 		goto fail;
 	}	
@@ -64,9 +56,8 @@ fail:
 static ssize_t hello_write(struct file *filp, const char __user *from, size_t length, loff_t *pos)
 {
 	char buf[BUF_SIZE] = {'\0'};
-	const dev_t *devp = filp->private_data;	
 	int result = -EINVAL;
-	size_t devno_len = snprintf(buf, sizeof(buf), "%d", MINOR(*devp));	
+	size_t devno_len = snprintf(buf, sizeof(buf), "%d", hello_mdev.minor);	
 
 	pr_debug("hello: User input %s\n", from);
 	result = memcmp(from, buf, 
