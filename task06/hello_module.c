029a7138c918843cf08e24f748240bc6e1b6420c
Vladimir Petrigo <taenaru@gmail.com>
2017-11-25 14:56:28 +0300

    task06: Add solution for task 06

--- task06/hello_module.c
+++ task06/hello_module.c
@@ -27,35 +27,32 @@ static struct miscdevice hello_mdev = {
     .minor = MISC_DYNAMIC_MINOR, .name = DEV_NAME, .fops = &hello_misc_ops};
 
 static const char assigned_id[] = "100f321669eb";
+#define ASSIGNED_ID_LEN (sizeof assigned_id)
 
 static ssize_t hello_read(struct file *filp, char __user *to, size_t length,
 			  loff_t *pos)
 {
-	ssize_t result = sizeof assigned_id;
-	if (copy_to_user(to, assigned_id, sizeof assigned_id)) {
-		result = -EFAULT;
-		goto fail;
-	}
-
-fail:
-	return result;
+	return simple_read_from_buffer(to, length, pos, assigned_id,
+				       ASSIGNED_ID_LEN);
 }
 
 static ssize_t hello_write(struct file *filp, const char __user *from,
 			   size_t length, loff_t *pos)
 {
-	char buf[sizeof assigned_id] = {'\0'};
-	ssize_t result;
+	char buf[ASSIGNED_ID_LEN] = {'\0'};
+	ssize_t result = -EINVAL;
 
-	if (length != (sizeof assigned_id) - 1 ||
-	    copy_from_user(buf, from, length) ||
-	    strncmp(assigned_id, buf, sizeof assigned_id)) {
-		result = -EINVAL;
-		goto fail;
+	if (length == ASSIGNED_ID_LEN - 1) {
+		pr_debug("%s: read user buffer\n", __func__);
+		result = simple_write_to_buffer(buf, ASSIGNED_ID_LEN, pos, from,
+						length);
 	}
 
-	result = sizeof assigned_id;
-fail:
+	if (result != -EINVAL &&
+	    strncmp(buf, assigned_id, ASSIGNED_ID_LEN) == 0) {
+		result = ASSIGNED_ID_LEN;
+	}
+
 	return result;
 }
 
